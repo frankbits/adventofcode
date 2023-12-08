d364679297f953cd879fbf9340ceeaab4d424007
Frank <62514408+frankbits@users.noreply.github.com>
2023-12-08 22:26:00 +0100

    added day 07

---
+++ 2023/07/py2.py
@@ -0,0 +1,67 @@
+import re
+
+
+def get_hand_type(cards):
+    card_counts = {}
+    for card in cards:
+        card_counts[card] = card_counts.get(card, 0) + 1
+    card_counts = dict(sorted(card_counts.items(), key=lambda item: -item[1]))
+
+    jokers = card_counts.pop('J', 0)
+    card_counts_values = list(card_counts.values())
+    try:
+        card_count = card_counts_values[0]
+    except IndexError:
+        card_count = 0
+    match card_count + jokers:
+        case 5:
+            return 5
+        case 4:
+            return 4
+        case 3:
+            if card_counts_values[1] == 2:
+                return 3.5  # full house
+            else:
+                return 3
+        case 2:
+            if card_counts_values[1] == 2:
+                return 2.5  # double pair
+            else:
+                return 2
+        case 1:
+            return 1
+
+
+file = 'input/input.txt'
+
+lines = list(map(lambda line: line.split(), open(file, "r").readlines()))
+
+hands = []
+for line in lines:
+    hand = re.findall(r".", line[0])
+    bid = line[1]
+
+    hands.append([hand, get_hand_type(hand), bid])
+
+cardRanks = {
+    'A': 14,
+    'K': 13,
+    'Q': 12,
+    'T': 10,
+    'J': 1,
+}
+
+
+def sort_hands(hand):
+    sort_tuple = [hand[1]]
+    for card in hand[0]:
+        sort_tuple.append(int(cardRanks.get(card, card)))
+    return sort_tuple
+
+
+hands = sorted(hands, key=sort_hands)
+
+out = 0
+for i, hand in enumerate(hands):
+    out += int(hand[2]) * (i + 1)
+print('out', out)
